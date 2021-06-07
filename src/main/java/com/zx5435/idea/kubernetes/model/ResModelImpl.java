5f877dc9d40c6989446d57884196c95e07c2948c
yujie <825407762@qq.com>
2021-06-07 23:02:13 +0800

    CronJob

--- src/main/java/com/zx5435/idea/kubernetes/model/ResModelImpl.java
+++ src/main/java/com/zx5435/idea/kubernetes/model/ResModelImpl.java
@@ -4,6 +4,7 @@ import com.zx5435.idea.kubernetes.node.*;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.batch.v1beta1.CronJob;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,6 +37,8 @@ public class ResModelImpl implements ResModel {
                 return listDp();
             case "Pod":
                 return listPod();
+            case "CronJob":
+                return listCronJob();
             default:
                 return Collections.emptyList();
         }
@@ -72,6 +75,17 @@ public class ResModelImpl implements ResModel {
         List<ITreeNode> ret = new ArrayList<>();
         for (Pod one : res) {
             ret.add(new PodNode(one));
+        }
+        return ret;
+    }
+
+    public List<ITreeNode> listCronJob() {
+        DefaultKubernetesClient client = new DefaultKubernetesClient();
+        List<CronJob> res = client.batch().v1beta1().cronjobs().inAnyNamespace().list().getItems();
+
+        List<ITreeNode> ret = new ArrayList<>();
+        for (CronJob one : res) {
+            ret.add(new CronJobNode(one));
         }
         return ret;
     }
